786ed4741dd6fddf19c7b08e9fce292de24c6cc4
mikeboensel <mikeboensel@users.noreply.github.com>
2026-04-10 18:24:43 -0400

    feat: add placeholder dense-context formula

---
+++ Formula/dense-context.rb
@@ -0,0 +1,10 @@
+class DenseContext < Formula
+  desc "Dense Context Systems CLI (placeholder)"
+  homepage "https://densecontext.co"
+  url "https://github.com/DenseContext/homebrew-tap/archive/refs/tags/v0.0.1.tar.gz"
+  version "0.0.1"
+
+  def install
+    # placeholder
+  end
+end
